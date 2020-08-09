73d4d02de6d228b5132a9031221dca856f1e366e
Saladin21 <ahmadsaladin21@gmail.com>
2020-08-09 16:48:52 +0700

    logout

--- index.php
+++ index.php
@@ -3,14 +3,17 @@ session_start();
 
 if(!isset($_SESSION['email'])){
     $_SESSION['msg'] = "Anda harus masuk untuk menggunakan fitur ini";
-    header("location : login.php");
+    header("location:login.php");
+}else{
+    if(isset($_GET['logout'])){
+        session_destroy();
+        unset($_SESSION['email']);
+        header("location:login.php");
+    }else{
+        include ('umkm_go.html');
+    }
 }
 
-if(isset($_GET['logout'])){
-    session_destroy();
-    unset($_SESSION['email']);
-    header("location : login.php");
-}
 
-include ('umkm_go.html');
+
 ?>
